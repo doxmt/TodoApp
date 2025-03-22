938dffce0bcb1df73f93d3fa82477e332ac85528
Songtaeha <songtaeha@naver.com>
2025-03-22 15:55:15 +0900

    기능 적용

--- redux/store.js
+++ redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import todoReducer from './slices/todoSlice';
 
-export const store = configureStore({
-  reducer:{
+const store = configureStore({
+  reducer: {
     todo: todoReducer
   }
 });
+
+export default store; 
